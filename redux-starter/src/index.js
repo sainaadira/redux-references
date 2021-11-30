09a36bd056505af920926e279cbbd616fa3b84c6
Sai <snaadira@gmail.com>
2021-11-30 16:03:00 -0500

    added completed code to implement adding,resolving and removing a bug

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,3 +1,25 @@
 import store from './store'
+import { bugAdded, bugRemoved, bugResolved } from './actions'
 
-console.log(store)
+// state = reducer(state, action) ** interal state of the store
+// notify the subscribers
+
+
+// function that gets called every time the state of store get changed
+// UI components should subscribe to the store so they get notified when the state of the store changes
+// subscribe method returns a function for unsubcribing from the store 
+const unsubscribe = store.subscribe(() => {
+  console.log('Store changed!', store.getState())
+})
+
+store.dispatch(bugAdded('Bug 1'))
+
+unsubscribe()
+
+store.dispatch(bugResolved(1))
+
+store.dispatch(bugRemoved)
+
+
+
+console.log(store.getState())
--- redux-starter/src/reducer.js
+++ redux-starter/src/reducer.js
@@ -1,8 +1,9 @@
-// []
+import * as actions from './actionTypes'
 
 let lastId = 0
+
 export default function reducer(state = [], action) {
-  if (action.type === 'bugAdded')
+  if (action.type === actions.BUG_ADDED)
     return [
       ...state,
       {
@@ -11,8 +12,14 @@ export default function reducer(state = [], action) {
         resolved: false
       }
     ]
-  else if (action.type === 'bugRemoved')
+  else if (action.type === actions.BUG_REMOVED)
     return state.filter(bug => bug.id !== action.payload.id)
+
+  else if (action.type === actions.BUG_RESOLVED)
+    // if the bug id does not equal to the bug that was resolved, return the bug or return a copy of that bug (new bug obj) where the resolved property is set to true
+    return state.map(bug =>
+      bug.id !== action.payload.id ? bug : { ...bug, resolved: true })
+
   // or return the current state
   return state
 }
